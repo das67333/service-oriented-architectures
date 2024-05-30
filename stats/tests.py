98d7b3096fd33e41706b10410c5830a3ee89b3b2
das67333 <spkazimov@edu.hse.ru>
2024-05-30 19:12:15 +0300

    Fix pipeline

--- stats/tests.py
+++ stats/tests.py
@@ -1,24 +1,31 @@
 import unittest
 from stats_pb2_grpc import ServiceStatsServicer
 
+class DummyClass:
+    def dummy_func(*args, **kwargs):
+        pass
+
+    def __getattribute__(self, name: str):
+        return DummyClass.dummy_func
+
 
 class TestServiceStatsServicer(unittest.TestCase):
     def test_get_post_stats(self):
         serv = ServiceStatsServicer()
         with self.assertRaises(NotImplementedError) as context:
-            serv.get_post_stats(None, None)
+            serv.get_post_stats(None, DummyClass())
         self.assertEqual('Method not implemented!', str(context.exception))
     
     def test_get_top_posts(self):
         serv = ServiceStatsServicer()
         with self.assertRaises(NotImplementedError) as context:
-            serv.get_top_posts(None, None)
+            serv.get_top_posts(None, DummyClass())
         self.assertEqual('Method not implemented!', str(context.exception))
     
     def test_get_top_users(self):
         serv = ServiceStatsServicer()
         with self.assertRaises(NotImplementedError) as context:
-            serv.get_top_users(None, None)
+            serv.get_top_users(None, DummyClass())
         self.assertEqual('Method not implemented!', str(context.exception))
 
 
