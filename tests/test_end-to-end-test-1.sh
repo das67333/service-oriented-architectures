e45ae79f726d9c73ab1f74e6088f7453f537714f
das67333 <spkazimov@edu.hse.ru>
2024-06-06 23:01:49 +0300

    Remove rubbish file

--- tests/test_end-to-end-test-1.sh
+++
@@ -1,7 +0,0 @@
-response_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST $HOST/signup -H "$SJ" -d "{ \"login\": \"$LOGIN\", \"password\": \"$PASSWORD\" }")
-
-if [ "$response_code" -eq 200 ]; then
-  echo "Test passed: Return code is 200"
-else
-  echo "Test failed: Return code is not 200"
-fi
